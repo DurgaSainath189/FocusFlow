ded2d85dbf020ed71cba38433626c561a78be2cc
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-20 23:36:30 +0530

    sign in component is done

--- app/[locale]/(auth)/sign-in/page.tsx
+++ app/[locale]/(auth)/sign-in/page.tsx
@@ -1,8 +1,14 @@
 import { AuthCard } from "@/components/auth/AuthCard";
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: "Sign In",
+  description: "Sign In Page",
+};
+
 const SignIn = () => {
-  return <AuthCard />;
+  return <AuthCard signInCard />;
 };
 
 export default SignIn;
--- app/[locale]/(auth)/sign-up/page.tsx
+++ app/[locale]/(auth)/sign-up/page.tsx
@@ -1,5 +1,11 @@
 import { AuthCard } from "@/components/auth/AuthCard";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Sign Up Page",
+};
 
 const SignUp = () => {
   return <AuthCard />;
