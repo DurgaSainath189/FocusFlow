99f299feddb14dc83a5cd04efa7ab14308c2eefb
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-22 00:24:05 +0530

    changed the layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -8,7 +8,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { AuthProvider } from "@/providers/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
 
-const locales = ["en"];
+const locales = ["en", "te","hi"];
 
 export const metadata: Metadata = {
   title: "Focus Flow",
@@ -26,7 +26,7 @@ export default async function RootLayout({
   const isValidLocale = locales.some((cur) => cur === locale);
   if (!isValidLocale) notFound();
 
-  const messages = await getMessages({ locale });
+  const messages = await getMessages(locale);
 
   return (
     <html lang={locale} suppressHydrationWarning>
