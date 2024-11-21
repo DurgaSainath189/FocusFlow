94b4c4d43dab2a13989bcf335977b97c9784c555
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-22 00:23:43 +0530

    added google and github providers

--- components/auth/ProviderSignInBtn.tsx
+++ components/auth/ProviderSignInBtn.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { useLocale } from "next-intl";
+import { useProviderLoginError } from "@/hooks/useProviderLoginError";
+import { signIn } from "next-auth/react";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  providerName: "google" | "github";
+  onLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const ProviderSignInBtn = ({ children, ...props }: Props) => {
+export const ProviderSignInBtn = ({
+  children,
+  providerName,
+  onLoading,
+  ...props
+}: Props) => {
+  const [showLoggedInfo, setShowLoggedInfo] = useState(false);
+  const locale = useLocale();
+  useProviderLoginError(showLoggedInfo);
+
+  const signInHandler = async () => {
+    onLoading(true);
+    setShowLoggedInfo(true);
+    try {
+      await signIn(providerName, { callbackUrl: `/${locale}/onboarding` });
+    } catch (error) {
+      console.log(error);
+    }
+    onLoading(false);
+  };
   return (
-    <Button {...props} variant={"secondary"} type="button">
+    <Button
+      {...props}
+      variant={"secondary"}
+      type="button"
+      onClick={signInHandler}
+    >
       {children}
     </Button>
   );
--- components/auth/ProviderSignInBtns.tsx
+++ components/auth/ProviderSignInBtns.tsx
@@ -7,15 +7,19 @@ import { useTranslations } from "next-intl";
 export const ProviderSignInBtns = ({
   signInCard,
   disabled,
+  onLoading,
 }: {
   signInCard?: boolean;
   disabled?: boolean;
+  onLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const t = useTranslations("AUTH");
   return (
     <div className="flex flex-col gap-2">
       <ProviderSignInBtn
         disabled={disabled}
+        onLoading={onLoading}
+        providerName="google"
         className="w-full rounded-[1.9rem] border text-sm h-12 sm:h-10 sm:text-base"
       >
         <GoogleLogo className="mr-2" width={20} height={20} />
@@ -23,17 +27,21 @@ export const ProviderSignInBtns = ({
           ? t("SIGN_IN.PROVIDERS.GOOGLE")
           : t("SIGN_UP.PROVIDERS.GOOGLE")}{" "}
       </ProviderSignInBtn>
-      <ProviderSignInBtn
+      {/* <ProviderSignInBtn
         disabled={disabled}
+        onLoading={onLoading}
+        providerName=""
         className="w-full bg-black/90 text-white dark:bg-black/70 hover:bg-black/80 dark:hover:bg-black/50 rounded-[1.9rem] border text-sm h-12 sm:h-10 sm:text-base"
       >
         <AppleLogo className="fill-white mr-2" width={20} height={20} />
         {signInCard
           ? t("SIGN_IN.PROVIDERS.APPLE")
           : t("SIGN_UP.PROVIDERS.APPLE")}{" "}
-      </ProviderSignInBtn>
+      </ProviderSignInBtn> */}
       <ProviderSignInBtn
         disabled={disabled}
+        onLoading={onLoading}
+        providerName="github"
         className="w-full rounded-[1.9rem] border text-sm h-12 sm:h-10 sm:text-base"
       >
         <GithubLogo className="fill-foreground mr-2" width={20} height={20} />
