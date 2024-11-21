1b15f8132f7f24ef6ea62765f54057dcef054805
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-21 23:50:50 +0530

    added the loading component

--- components/ui/loadingState.tsx
+++ components/ui/loadingState.tsx
@@ -1,15 +1,27 @@
+import { cn } from "@/lib/utils";
+import { SvgProps } from "@/types/props";
 import { Loader2 } from "lucide-react";
 
-interface Props {
-  loadingText: string;
+interface Props extends SvgProps {
+  loadingText?: string;
   hideLoaderIcon?: boolean;
 }
 
-export const LoadingState = ({ loadingText, hideLoaderIcon }: Props) => {
+export const LoadingState = ({
+  loadingText,
+  hideLoaderIcon = false,
+  className,
+  ...props
+}: Props) => {
   return (
     <>
-      {!hideLoaderIcon && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-      <p>{loadingText}</p>
+      {!hideLoaderIcon && (
+        <Loader2
+          className={cn(`mr-2 h-4 w-4 animate-spin`, className)}
+          {...props}
+        />
+      )}
+      {loadingText && <p>{loadingText}</p>}
     </>
   );
 };
