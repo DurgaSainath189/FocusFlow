2fe8a486408a30c868d9fabeada8b8713b7b893c
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-23 00:30:45 +0530

    some changes

--- lib/checkIfUserCompletedOnboarding.ts
+++ lib/checkIfUserCompletedOnboarding.ts
@@ -3,6 +3,8 @@ import { getAuthSession } from "./auth";
 
 export const checkIfUserCompletedOnboarding = async (currentPath: string) => {
   const session = await getAuthSession();
+  console.log(`session:${session}`);
+
   if (!session) redirect("/");
   if (session.user.completedOnboarding && currentPath === "/onboarding")
     redirect("/dashboard");
