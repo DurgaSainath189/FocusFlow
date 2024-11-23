034e7e838a282465a59a31ff562e13c41dddc5ae
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-24 00:10:23 +0530

    added some image configurations

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"github.com"
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 const withNextIntl = require("next-intl/plugin")("./i18n.ts");
