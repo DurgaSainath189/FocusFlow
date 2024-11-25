caf09a6b9fc2b11d75463076b9cee6be910d147f
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-25 23:42:12 +0530

    made some changes

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { signOut, useSession } from "next-auth/react";
 import { useTranslations } from "next-intl";
 
+
 const Home = () => {
   const t = useTranslations("Index");
   const session = useSession();
