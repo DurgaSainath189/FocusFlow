c87bb46ceef904969cfbf5d0597df98584775594
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-22 01:50:51 +0530

    made some changes

--- lib/uploadingthing.ts
+++
@@ -1,7 +0,0 @@
-import { generateReactHelpers } from "@uploadthing/react";
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
-import { generateUploadButton } from "@uploadthing/react";
-
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>();
-export const UploadButton = generateUploadButton<OurFileRouter>();
