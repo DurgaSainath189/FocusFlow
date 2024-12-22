de07b508206b9d0b4089f01c629ec4fc147ce30b
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-22 23:35:55 +0530

    added the theme page

--- app/[locale]/dashboard/settings/theme/page.tsx
+++ app/[locale]/dashboard/settings/theme/page.tsx
@@ -1,10 +1,11 @@
+import { Theme } from "@/components/settings/theme/Theme";
 import { checkIfUserCompletedOnboarding } from "@/lib/checkIfUserCompletedOnboarding";
 import React from "react";
 
 const ThemeSettings = async () => {
   const session = await checkIfUserCompletedOnboarding("/dashboard/settings");
 
-  return <div>ThemeSettings</div>;
+  return <Theme />;
 };
 
 export default ThemeSettings;
