2e4b8eb796ad0a80b252eed6b0332fda36fed80d
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-22 18:02:36 +0530

    added main sidebar component

---
+++ components/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { OptionsSidebar } from "./optionsSidebar/OptionsSidebar";
+import { ShortcutSidebar } from "./shortcutSidebar/ShortcutSidebar";
+
+export const Sidebar = () => {
+  return (
+    <aside className="fixed top-0 left-0 md:static h-full flex overflow-hidden">
+      <ShortcutSidebar />
+      <OptionsSidebar />
+    </aside>
+  );
+};
