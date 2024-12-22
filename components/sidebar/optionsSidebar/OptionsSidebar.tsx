2ea2438f97910725cfead9e0b0abc4ca7323a162
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-22 18:02:26 +0530

    added new optionsidebar components

---
+++ components/sidebar/optionsSidebar/OptionsSidebar.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Settings } from "./Settings";
+
+export const OptionsSidebar = () => {
+  const pathname = usePathname();
+  if (pathname === "/dashboard") return null;
+
+  const urlWorkspaceId: string | undefined = pathname.split("/")[3];
+  const urlAdditionalId: string | undefined = pathname.split("/")[6];
+  const chatId: string | undefined = pathname.split("/")[5];
+  const workspaceId = urlWorkspaceId ? urlWorkspaceId : "";
+
+  if (
+    pathname === "/dashboard" ||
+    pathname === "/dashboard/starred" ||
+    pathname === "/dashboard/calendar" ||
+    (urlAdditionalId &&
+      pathname ===
+        `/dashboard/workspace/${workspaceId}/tasks/task/${urlAdditionalId}/edit`) ||
+    (urlAdditionalId &&
+      pathname ===
+        `/dashboard/workspace/${workspaceId}/mind-maps/mind-map/${urlAdditionalId}/edit`)
+  ) {
+    return null;
+  }
+  return (
+    <div className="border-r sm:w-64 w-52 h-full p-4 sm:py-6 flex flex-col justify-between">
+      {pathname.includes("/dashboard/settings") && <Settings />}
+      {/* {(pathname === `/dashboard/workspace/${workspaceId}` ||
+            pathname ===
+              `/dashboard/workspace/${workspaceId}/tasks/task/${urlAdditionalId}` ||
+            pathname ===
+              `/dashboard/workspace/${workspaceId}/mind-maps/mind-map/${urlAdditionalId}` ||
+            pathname ===
+              `/dashboard/workspace/${workspaceId}/chat/${chatId}`) && (
+            <WorkspaceOptions workspaceId={workspaceId} />
+          )}
+  
+          {(pathname === "/dashboard/pomodoro" ||
+            pathname === "/dashboard/pomodoro/settings") && <PomodoroLinks />}
+  
+          {pathname === "/dashboard/assigned-to-me" && (
+            <AssignedToMeFilter userWorkspaces={userWorkspaces} />
+          )} */}
+    </div>
+  );
+};
