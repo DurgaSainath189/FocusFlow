8a166870175907e064255e66ad98abca718e2bbc
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-22 18:02:13 +0530

    added new sidebar components

---
+++ components/sidebar/shortcutSidebar/ShortcutSidebar.tsx
@@ -0,0 +1,21 @@
+import { Workspace } from "@prisma/client";
+import { Top } from "./Top";
+import { Bottom } from "./Bottom";
+import { Workspaces } from "./Workspaces";
+
+// interface Props {
+//   userWorkspaces: Workspace[];
+//   createdWorkspaces: number;
+// }
+
+export const ShortcutSidebar = () => {
+  return (
+    <div className="border-r h-full flex flex-col justify-between items-center p-4 sm:py-6">
+      <div className="w-full h-2/3">
+        <Top />
+        <Workspaces />
+      </div>
+      <Bottom />
+    </div>
+  );
+};
---
+++ components/sidebar/shortcutSidebar/Top.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import ActiveLink from "@/components/ui/active-link";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Home } from "lucide-react";
+import { useTranslations } from "next-intl";
+
+export const Top = () => {
+  const t = useTranslations("SIDEBAR");
+  return (
+    <div className="flex flex-col gap-4">
+      <HoverCard openDelay={250} closeDelay={250}>
+        <HoverCardTrigger asChild>
+          <ActiveLink variant={"ghost"} size={"icon"} href={"/dashboard"}>
+            <Home />
+          </ActiveLink>
+        </HoverCardTrigger>
+        <HoverCardContent align="start">
+          <span>{t("MAIN.HOME_HOVER")}</span>
+        </HoverCardContent>
+      </HoverCard>
+    </div>
+  );
+};
