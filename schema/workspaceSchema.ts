ac2373ccc0e5e4abffc1806b18a090eac388ac3f
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-22 15:57:01 +0530

    added workspace schema

--- schema/workspaceSchema.ts
+++ schema/workspaceSchema.ts
@@ -91,21 +91,21 @@ import { MAX_FILE_SIZE, ACCEPTED_IMAGE_TYPES } from "./imageSchema";
 // export type WorkspaceSchema = z.infer<typeof workspaceSchema>;
 
 export const workspaceSchema = z.object({
-  name: z
+  workspaceName: z
     .string()
-    .min(2, "SCHEMA.USERNAME.SHORT")
+    .min(2, "SCHEMA.WORKSPACE.SHORT")
     .refine((username) => /^[a-zA-Z0-9]+$/.test(username), {
-      message: "SCHEMA.USERNAME.SPECIAL_CHARS",
+      message: "SCHEMA.WORKSPACE.SPECIAL_CHARS",
     }),
   file: z
     .any()
-    .optional()
-    .nullable()
     .refine((file) => file?.size <= MAX_FILE_SIZE, "SCHEMA.IMAGE.MAX")
     .refine(
       (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
       "SCHEMA.IMAGE.SUPPORTED"
-    ),
+    )
+    .optional()
+    .nullable(),
 });
 
 export type WorkspaceSchema = z.infer<typeof workspaceSchema>;
