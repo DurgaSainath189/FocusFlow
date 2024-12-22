318c3d190454f34f0259d0219c3ce26bd954bb95
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-22 23:36:06 +0530

    added the settings page

--- app/[locale]/dashboard/settings/page.tsx
+++ app/[locale]/dashboard/settings/page.tsx
@@ -1,10 +1,17 @@
+import { AccountInfo } from "@/components/settings/account/AccountInfo";
+import { Heading } from "@/components/settings/account/Heading";
 import { checkIfUserCompletedOnboarding } from "@/lib/checkIfUserCompletedOnboarding";
 import React from "react";
 
 const Settings = async () => {
   const session = await checkIfUserCompletedOnboarding("/dashboard/settings");
 
-  return <div>Settings</div>;
+  return (
+    <div>
+      <Heading />
+      <AccountInfo session={session} />
+    </div>
+  );
 };
 
 export default Settings;
