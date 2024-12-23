4e293b1880647aab9c94941c680ccc9c96acf987
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-23 21:52:32 +0530

    added the page for security

--- app/[locale]/dashboard/settings/security/page.tsx
+++ app/[locale]/dashboard/settings/security/page.tsx
@@ -1,10 +1,11 @@
+import { SecurityCard } from "@/components/settings/security/SecurityCard";
 import { checkIfUserCompletedOnboarding } from "@/lib/checkIfUserCompletedOnboarding";
 import React from "react";
 
 const SecuritySettings = async () => {
   const session = await checkIfUserCompletedOnboarding("/dashboard/settings");
 
-  return <div>SecuritySettings</div>;
+  return <SecurityCard />;
 };
 
 export default SecuritySettings;
