19f8fcd7db027e382b7cbe2a96d13c9a5b206840
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-23 20:54:22 +0530

    modified the settings page

--- app/[locale]/dashboard/settings/page.tsx
+++ app/[locale]/dashboard/settings/page.tsx
@@ -1,5 +1,7 @@
 import { AccountInfo } from "@/components/settings/account/AccountInfo";
+import { DeleteAccount } from "@/components/settings/account/DeleteAccount";
 import { Heading } from "@/components/settings/account/Heading";
+import { Separator } from "@/components/ui/separator";
 import { checkIfUserCompletedOnboarding } from "@/lib/checkIfUserCompletedOnboarding";
 import React from "react";
 
@@ -10,6 +12,10 @@ const Settings = async () => {
     <div>
       <Heading />
       <AccountInfo session={session} />
+      <div className="p-4 sm:p-6">
+        <Separator />
+      </div>
+      <DeleteAccount />
     </div>
   );
 };
