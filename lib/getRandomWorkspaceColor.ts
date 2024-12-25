8bdc0029a2875dcac2f1ded9ea31f9a52f4e70fe
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-26 01:09:28 +0530

    added api to edit, delete the workspaces

--- lib/getRandomWorkspaceColor.ts
+++ lib/getRandomWorkspaceColor.ts
@@ -1,5 +1,7 @@
 import { WorkspaceIconColor } from "@prisma/client";
 
+export const colors = Object.values(WorkspaceIconColor);
+
 export const getRandomWorkspaceColor = () => {
   const colors: string[] = Object.values(WorkspaceIconColor);
   const randomIndex = Math.floor(Math.random() * colors.length);
