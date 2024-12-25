0205578a7872a6bd681fde99fe82faf939589aac
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-25 19:01:33 +0530

    added colors and changed the api path

---
+++ lib/getRandomWorkspaceColor.ts
@@ -0,0 +1,7 @@
+import { WorkspaceIconColor } from "@prisma/client";
+
+export const getRandomWorkspaceColor = () => {
+  const colors: string[] = Object.values(WorkspaceIconColor);
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex] as WorkspaceIconColor;
+};
