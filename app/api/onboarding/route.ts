877f4194fd7b0ccf94d1d9ef43c51a8cc78db4df
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-26 01:12:24 +0530

    changed the user permissions

--- app/api/onboarding/route.ts
+++ app/api/onboarding/route.ts
@@ -64,7 +64,7 @@ export async function POST(request: Request) {
       data: {
         userId: user.id,
         workspaceId: workspace.id,
-        userRole: "ADMIN",
+        userRole: "OWNER",
       },
     });
 
