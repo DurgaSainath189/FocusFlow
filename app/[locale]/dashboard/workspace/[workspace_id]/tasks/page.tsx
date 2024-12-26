52983193020bf6660348571ee0334133bc282840
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-26 20:24:21 +0530

    added the tasks page and loader

---
+++ app/[locale]/dashboard/workspace/[workspace_id]/tasks/page.tsx
@@ -0,0 +1,37 @@
+import { DashboardHeader } from "@/components/header/DashboardHeader";
+import { InviteUsers } from "@/components/inviteUsers/InviteUsers";
+import { NewTask } from "@/components/tasks/newTask/NewTask";
+import { getUserWorkspaceRole, getWorkspace } from "@/lib/api";
+import { checkIfUserCompletedOnboarding } from "@/lib/checkIfUserCompletedOnboarding";
+
+interface Params {
+  params: {
+    workspace_id: string;
+  };
+}
+
+const Tasks = async ({ params: { workspace_id } }: Params) => {
+  const session = await checkIfUserCompletedOnboarding(
+    `/dashboard/workspace/${workspace_id}`
+  );
+
+  const [workspace, userRole] = await Promise.all([
+    getWorkspace(workspace_id, session.user.id),
+    getUserWorkspaceRole(workspace_id, session.user.id),
+  ]);
+
+  return (
+    <>
+      <DashboardHeader addManualRoutes={["dashboard", workspace.name]}>
+        {(userRole === "ADMIN" || userRole === "OWNER") && (
+          <InviteUsers workspace={workspace} />
+        )}
+      </DashboardHeader>
+      <main className="flex flex-col gap-2 min-h-[40rem]">
+        <NewTask />
+      </main>
+    </>
+  );
+};
+
+export default Tasks;
