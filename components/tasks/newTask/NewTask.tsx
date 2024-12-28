9b5d1be9bda384342f41affd848b23f80f8720c5
DurgaSainath189 <durgasainathdora@gmail.com>
2024-12-29 02:05:21 +0530

    edited some part of the new tag component

--- components/tasks/newTask/NewTask.tsx
+++ components/tasks/newTask/NewTask.tsx
@@ -1,5 +1,9 @@
 import { Editor } from "@/components/editor/Editor";
 
-export const NewTask = () => {
-  return <Editor />;
+interface Props {
+  workspaceId: string;
+}
+
+export const NewTask = ({ workspaceId }: Props) => {
+  return <Editor workspaceId={workspaceId} initialActiveTags={[]} />;
 };
