6cdfe2ed48ad08b766aa5ba0bdbbb3e0ee6604e6
DurgaSainath189 <durgasainathdora@gmail.com>
2025-01-04 20:19:57 +0530

    added autosave provider

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,16 +1,19 @@
 import { Sidebar } from "@/components/sidebar/Sidebar";
+import { SaveTaskStateProvider } from "@/context/SaveTaskState";
 import { ToggleSidebarProvider } from "@/context/ToggleSidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ToggleSidebarProvider>
-      <div className="flex h-0 min-h-screen w-full overflow-hidden">
-        <Sidebar />
-        <div className="relative p-4 md:p-6 lg:px-10 flex-grow flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-muted-foreground scrollbar-track-secondary">
-          {children}
+    <SaveTaskStateProvider>
+      <ToggleSidebarProvider>
+        <div className="flex h-0 min-h-screen w-full overflow-hidden">
+          <Sidebar />
+          <div className="relative p-4 md:p-6 lg:px-10 flex-grow flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-muted-foreground scrollbar-track-secondary">
+            {children}
+          </div>
         </div>
-      </div>
-    </ToggleSidebarProvider>
+      </ToggleSidebarProvider>
+    </SaveTaskStateProvider>
   );
 };
 
