6e170d4ecfb9d830342bc6794b6921c18cf60238
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-01-05 23:25:19 +0530

    added new schema

---
+++ prisma/migrations/20250105153838_added_saved_mindmaps_model/migration.sql
@@ -0,0 +1,35 @@
+-- DropForeignKey
+ALTER TABLE "Task" DROP CONSTRAINT "Task_creatorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Task" DROP CONSTRAINT "Task_updatedUserId_fkey";
+
+-- AlterTable
+ALTER TABLE "MindMap" ALTER COLUMN "emoji" SET DEFAULT '1f4d2';
+
+-- CreateTable
+CREATE TABLE "savedMindMaps" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "taskId" TEXT NOT NULL,
+
+    CONSTRAINT "savedMindMaps_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "savedMindMaps_userId_idx" ON "savedMindMaps"("userId");
+
+-- CreateIndex
+CREATE INDEX "savedMindMaps_taskId_idx" ON "savedMindMaps"("taskId");
+
+-- AddForeignKey
+ALTER TABLE "savedMindMaps" ADD CONSTRAINT "savedMindMaps_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "savedMindMaps" ADD CONSTRAINT "savedMindMaps_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "MindMap"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_updatedUserId_fkey" FOREIGN KEY ("updatedUserId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
