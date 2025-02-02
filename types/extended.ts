fc8e0e70c99d38095dc437f74e619e3967e2418c
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-02 22:57:01 +0530

    added some types

--- types/extended.ts
+++ types/extended.ts
@@ -93,3 +93,26 @@ export interface AssignedToMindMapUser {
 export interface UsersAssignedToMindMapInfo extends Workspace {
   subscribers: AssignedToMindMapUser[];
 }
+
+export type AssignedItemType = "task" | "mindMap";
+
+export interface AssignedToMeDataItem {
+  id: string;
+  title: string;
+  emoji: string;
+  link: string;
+  workspaceName: string;
+  createdAt: Date;
+  type: AssignedItemType;
+  updated: {
+    at: Date;
+    by?: UserInfo | null;
+  };
+  workspaceId: string;
+  starred: boolean;
+}
+
+export interface AssignedToMeTaskAndMindMaps {
+  tasks: AssignedToMeDataItem[];
+  mindMaps: AssignedToMeDataItem[];
+}
