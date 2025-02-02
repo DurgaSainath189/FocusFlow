f881597fe93fb99e2d3759d9e037fe80413a5345
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-02 19:40:04 +0530

    made the sidebar into reusable component

--- components/sidebar/shortcutSidebar/Top.tsx
+++ components/sidebar/shortcutSidebar/Top.tsx
@@ -1,59 +1,21 @@
 "use client";
 
-import ActiveLink from "@/components/ui/active-link";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
-import { BrainCircuit, CalendarDays, Home } from "lucide-react";
-import { useTranslations } from "next-intl";
+import { topSidebarLinks } from "@/lib/utils";
+
+import { SidebarLink } from "./SidebarLink";
 
 export const Top = () => {
-  const t = useTranslations("SIDEBAR");
   return (
-    <div className="flex flex-col gap-4 items-center">
-      <HoverCard openDelay={250} closeDelay={250}>
-        <HoverCardTrigger asChild>
-          <ActiveLink variant={"ghost"} size={"icon"} href={"/dashboard"}>
-            <Home />
-          </ActiveLink>
-        </HoverCardTrigger>
-        <HoverCardContent align="start">
-          <span>{t("MAIN.HOME_HOVER")}</span>
-        </HoverCardContent>
-      </HoverCard>
-
-      <HoverCard openDelay={250} closeDelay={250}>
-        <HoverCardTrigger asChild>
-          <ActiveLink
-            include="pomodoro"
-            variant={"ghost"}
-            size={"icon"}
-            href={"/dashboard/pomodoro"}
-          >
-            <BrainCircuit />
-          </ActiveLink>
-        </HoverCardTrigger>
-        <HoverCardContent align="start">
-          <span>Pomodoro</span>
-        </HoverCardContent>
-      </HoverCard>
-
-      <HoverCard openDelay={250} closeDelay={250}>
-        <HoverCardTrigger asChild>
-          <ActiveLink
-            variant={"ghost"}
-            size={"icon"}
-            href={"/dashboard/calendar"}
-          >
-            <CalendarDays />
-          </ActiveLink>
-        </HoverCardTrigger>
-        <HoverCardContent align="start">
-          <span>Calendar</span>
-        </HoverCardContent>
-      </HoverCard>
+    <div className="flex flex-col items-center gap-3">
+      {topSidebarLinks.map((link, i) => (
+        <SidebarLink
+          key={`link_${i}`}
+          Icon={link.Icon}
+          hoverTextKey={link.hoverTextKey}
+          href={link.href}
+          include={link?.include}
+        />
+      ))}
     </div>
   );
 };
