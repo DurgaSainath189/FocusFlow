0a56f1544e03602991ba4772bdf5d56c3052924c
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-04 18:56:18 +0530

    added  addtaskshortcut component

--- app/[locale]/dashboard/pomodoro/settings/page.tsx
+++ app/[locale]/dashboard/pomodoro/settings/page.tsx
@@ -1,8 +1,8 @@
+import { AddTaskShortcut } from "@/components/addTaskShortCut/AddTaskShortcut";
 import { DashboardHeader } from "@/components/header/DashboardHeader";
 import { SettingsContainer } from "@/components/pomodoro/SettingsContainer";
 import { getUserPomodoroSettings } from "@/lib/api";
 import { checkIfUserCompletedOnboarding } from "@/lib/checkIfUserCompletedOnboarding";
-// import { AddTaskShortcut } from "@/components/addTaskShortCut/AddTaskShortcut";
 
 const PomodoroSettings = async () => {
   const session = await checkIfUserCompletedOnboarding(`/dashboard/pomodoro`);
@@ -12,7 +12,7 @@ const PomodoroSettings = async () => {
   return (
     <>
       <DashboardHeader>
-        {/* <AddTaskShortcut userId={session.user.id} /> */}
+        <AddTaskShortcut userId={session.user.id} />
       </DashboardHeader>
       <main className="flex flex-col gap-2 h-full">
         <SettingsContainer pomodoroSettings={pomodoroSettings} />
