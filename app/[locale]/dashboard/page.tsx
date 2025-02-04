bbae519c0b1c9a83b231bddd009304e74911b768
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-04 18:57:27 +0530

    made some changes to the dashboard page and layout

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,26 +1,34 @@
 import Welcoming from "@/components/common/Welcoming";
 import { DashboardHeader } from "@/components/header/DashboardHeader";
+import { HomeRecentActivityContainer } from "@/components/homeRecentActivity/HomeRecentActivityContainer";
+import { getInitialHomeRecentActivity } from "@/lib/api";
 import { checkIfUserCompletedOnboarding } from "@/lib/checkIfUserCompletedOnboarding";
-import React from "react";
 
-const DashBoard = async () => {
+const Dashboard = async () => {
   const session = await checkIfUserCompletedOnboarding("/dashboard");
-  console.log(session);
+
+  const initialRecentActivity = await getInitialHomeRecentActivity(
+    session.user.id
+  );
 
   return (
     <>
       <DashboardHeader />
-      <main>
+      <main className="h-full w-full">
         <Welcoming
           hideOnDesktop
           className="px-4 py-2"
-          username={session?.user.username!}
-          name={session?.user.name}
-          surname={session?.user.surname}
+          username={session.user.username!}
+          name={session.user.name}
+          surname={session.user.surname}
+        />
+        <HomeRecentActivityContainer
+          userId={session.user.id}
+          initialData={initialRecentActivity ? initialRecentActivity : []}
         />
       </main>
     </>
   );
 };
 
-export default DashBoard;
+export default Dashboard;
