623edc86ded3f2096a8bd8ebbd61a441aebf82d3
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-05 23:59:34 +0530

    added header to the home page

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -1,9 +1,13 @@
+import { Header } from "./header/Header";
 import { Nav } from "./nav/Nav";
 
 export const HomePage = () => {
   return (
     <>
       <Nav />
+      <div className="w-full mx-auto max-w-screen-xl px-2 sm:px-6">
+        <Header />
+      </div>
     </>
   );
 };
