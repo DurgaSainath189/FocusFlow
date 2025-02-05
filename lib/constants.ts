9430e38bccf25a8b1bf426ad4d937ef11d9447d9
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-05 23:59:22 +0530

    header links of home are added in the constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,38 @@
+import {
+  CalendarDays,
+  Clock,
+  MessageSquare,
+  PencilRuler,
+  Workflow,
+} from "lucide-react";
+
 export const ACTIVITY_PER_PAGE = 8;
 export const MESSAGES_LIMIT = 30;
+
+export const homePageHeaderLinks = [
+  {
+    href: "Tasks",
+    Icon: PencilRuler,
+    title: "Tasks & Notes",
+  },
+  {
+    href: "Mind-Maps",
+    Icon: Workflow,
+    title: "Mind Maps",
+  },
+  {
+    href: "Calendar",
+    Icon: CalendarDays,
+    title: "Calendar",
+  },
+  {
+    href: "Chat",
+    Icon: MessageSquare,
+    title: "Group Chat",
+  },
+  {
+    href: "Pomodoro",
+    Icon: Clock,
+    title: "Pomodoro",
+  },
+];
