9883be6f84639395b00508181d25f83c99400fda
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-06 19:29:02 +0530

    removed unwanted imports

--- app/[locale]/onboarding/page.tsx
+++ app/[locale]/onboarding/page.tsx
@@ -2,11 +2,9 @@ import { AdditionalInfoSection } from "@/components/onboarding/AdditionalInfoSec
 import { SummarySection } from "@/components/onboarding/SummarySection";
 import { OnboardingFormProvider } from "@/context/OnboardingForm";
 import { checkIfUserCompletedOnboarding } from "@/lib/checkIfUserCompletedOnboarding";
-import React from "react";
 
 const Onboarding = async () => {
   const session = await checkIfUserCompletedOnboarding("/onboarding");
-  // console.log(session);
 
   return (
     <OnboardingFormProvider session={session}>
