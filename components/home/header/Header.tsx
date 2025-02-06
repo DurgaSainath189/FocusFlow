9d83febeee24ac3a32a2019d8b270f51adb1b76d
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-06 18:27:23 +0530

    header components for the home screen are done

--- components/home/header/Header.tsx
+++ components/home/header/Header.tsx
@@ -1,12 +1,17 @@
 "use client";
-import { homePageHeaderLinks } from "@/lib/constants";
+import { homePageHeaderImgs, homePageHeaderLinks } from "@/lib/constants";
 import { HeaderLink } from "./HeaderLink";
-import { VideoContainer } from "../video/VideoContainer";
+import { ImagesCarousel } from "../carousel/ImagesCarousel";
+import { useIsVisible } from "@/hooks/useIsVisible";
 
 export const Header = () => {
+  const { isVisible, ref } = useIsVisible();
   return (
     <header className="flex flex-col items-center mt-20 w-full relative isolate group">
-      <h1 className="font-bold text-5xl sm:text-6xl lg:text-8xl max-w-2xl text-center">
+      <h1
+        ref={ref}
+        className="font-bold text-5xl sm:text-6xl lg:text-8xl max-w-2xl text-center"
+      >
         Your Ultimate Productive App
       </h1>
       <div className="w-full flex flex-wrap items-center justify-center mt-12 gap-2 sm:gap-4">
@@ -20,54 +25,11 @@ export const Header = () => {
         ))}
       </div>
 
-      <VideoContainer className="mt-16 h-[40rem] z-20" />
+      <ImagesCarousel
+        images={homePageHeaderImgs}
+        className="mt-28 h-[40rem] z-20 relative bg-background"
+      />
 
-      <div className="flex flex-col items-center mt-16 sm:mt-20 md:mt-24 lg:mt-32 w-full px-2 relative isolate text-center">
-        <h2 className="font-bold lg:text-8xl text-5xl sm:text-6xl text-center">
-          Your new best buddy
-        </h2>
-        <p className="lg:text-2xl text-base mt-9 sm:mt-4 sm:text-lg md:text-xl text-muted-foreground">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi
-          officiis, modi obcaecati enim quidem quibusdam quasi laboriosam rem!
-          Porro necessitatibus itaque nobis tenetur nihil amet asperiores ad
-          rerum temporibus explicabo.
-        </p>
-
-        <div
-          aria-hidden="true"
-          className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className={`relative left-[calc(50%+11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#e74b4b] to-[#a50505]  sm:left-[calc(50%-3rem)] sm:w-[72.1875rem] group-hover:opacity-80 dark:group-hover:opacity-60 transition-opacity duration-500 ${
-              // isVisible
-              //   ? "opacity:80 dark:opacity-60"
-              //   : "opacity-40 dark:opacity-30"
-              "opacity-40 dark:opacity-30"
-            }`}
-          />
-        </div>
-        <div
-          aria-hidden="true"
-          className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className={`relative left-[calc(50%+11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#e74b4b] to-[#a50505]  sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] group-hover:opacity-80 dark:group-hover:opacity-60 transition-opacity duration-500 ${
-              // isVisible
-              //   ? "opacity:80 dark:opacity-60"
-              //   : "opacity-40 dark:opacity-30"
-              "opacity-40 dark:opacity-30"
-            }`}
-          />
-        </div>
-      </div>
       <div
         aria-hidden="true"
         className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -78,27 +40,25 @@ export const Header = () => {
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
           className={`relative left-[calc(50%+11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#e74b4b] to-[#a50505]  sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] group-hover:opacity-80 dark:group-hover:opacity-60 transition-opacity duration-500 ${
-            // isVisible
-            //   ? "opacity:80 dark:opacity-60"
-            //   : "opacity-40 dark:opacity-30"
-            "opacity-40 dark:opacity-30"
+            isVisible
+              ? "opacity:80 dark:opacity-60"
+              : "opacity-40 dark:opacity-30"
           }`}
         />
       </div>
       <div
         aria-hidden="true"
-        className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-36 "
       >
         <div
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className={`relative left-[calc(50%+11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#e74b4b] to-[#a50505]  sm:left-[calc(50%-3rem)] sm:w-[72.1875rem] group-hover:opacity-80 dark:group-hover:opacity-60 transition-opacity duration-500 ${
-            // isVisible
-            //   ? "opacity:80 dark:opacity-60"
-            //   : "opacity-40 dark:opacity-30"
-            "opacity-40 dark:opacity-30"
+          className={`relative left-[calc(50%+11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#e74b4b] to-[#a50505]  sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] group-hover:opacity-80 dark:group-hover:opacity-60 transition-opacity duration-500 ${
+            isVisible
+              ? "opacity:80 dark:opacity-60"
+              : "opacity-40 dark:opacity-30"
           }`}
         />
       </div>
--- components/home/header/HeaderLink.tsx
+++ components/home/header/HeaderLink.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { scrollToHash } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
-import Link from "next/link";
 
 interface Props {
   Icon: LucideIcon;
@@ -9,11 +10,14 @@ interface Props {
 
 export const HeaderLink = ({ Icon, href, title }: Props) => {
   return (
-    <Link className="" href={"/"}>
-      <div className="text-secondary-foreground p-4 h-24 w-40 rounded-md gap-4 hover:bg-accent/50 flex flex-col justify-center items-center bg-transparent transition-colors duration-200">
-        <Icon />
-        <p>{title}</p>
-      </div>
-    </Link>
+    <Button
+      onClick={() => {
+        scrollToHash(href);
+      }}
+      className="text-secondary-foreground p-4 h-24 w-40 rounded-md gap-4 hover:bg-accent/50 flex flex-col justify-center items-center bg-transparent transition-colors duration-200"
+    >
+      <Icon />
+      <p>{title}</p>
+    </Button>
   );
 };
