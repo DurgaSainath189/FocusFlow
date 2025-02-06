da25819c8bbd0d444e98478ce8b8db4dd42ade59
ChandanaSreeP <chandanasree_p@srmap.edu.in>
2025-02-06 22:33:22 +0530

    made some random changes

--- lib/changeCodeToEmoji.ts
+++ lib/changeCodeToEmoji.ts
@@ -1,4 +1,4 @@
 export const changeCodeToEmoji = (code: string) => {
-    const isValidHex = /^[0-9a-fA-F]+$/.test(code);
-    return isValidHex ? String.fromCodePoint(parseInt(code, 16)) : "1f4d2";
-  };
+  const isValidHex = /^[0-9a-fA-F]+$/.test(code);
+  return isValidHex ? String.fromCodePoint(parseInt(code, 16)) : "1f4d2";
+};
