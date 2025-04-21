f7cd0f0ea414a079d6d10dffd44660a985609b89
DurgaSainath189 <durgasainathdora@gmail.com>
2025-04-22 00:13:34 +0530

    changed prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 
+// declare global {
+//   var prisma: PrismaClient;
+// }
+
+// let prisma: PrismaClient;
+
+// if (!global.prisma) {
+//   global.prisma = new PrismaClient();
+// }
+// prisma = global.prisma;
+
+// export const db = prisma;
+
+
+// import { PrismaClient } from '@/generated/prisma-client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
 declare global {
-  var prisma: PrismaClient;
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-let prisma: PrismaClient;
-
-if (!global.prisma) {
-  global.prisma = new PrismaClient();
-}
-prisma = global.prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 export const db = prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
