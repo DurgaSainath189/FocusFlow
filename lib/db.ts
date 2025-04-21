ec7b92e3a3ebaa7f7b1b595b606c61439e1306dd
DurgaSainath189 <durgasainathdora@gmail.com>
2025-04-22 01:17:28 +0530

    revert back the prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -1,31 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-// declare global {
-//   var prisma: PrismaClient;
-// }
-
-// let prisma: PrismaClient;
-
-// if (!global.prisma) {
-//   global.prisma = new PrismaClient();
-// }
-// prisma = global.prisma;
-
-// export const db = prisma;
-
-
-// import { PrismaClient } from '@/generated/prisma-client'
-
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+  var prisma: PrismaClient;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+let prisma: PrismaClient;
+
+if (!global.prisma) {
+  global.prisma = new PrismaClient();
+}
+prisma = global.prisma;
 
 export const db = prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
