e87395c0f76827943f4df7d428015b14c3ef8848
DurgaSainath189 <durgasainathdora@gmail.com>
2025-04-22 09:13:15 +0530

    changed the production link

--- lib/api.ts
+++ lib/api.ts
@@ -17,7 +17,7 @@ import { ACTIVITY_PER_PAGE } from "./constants";
 export const domain =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "http://localhost:3000";
+    : "http://focus-flow-nine.vercel.app";
 
 export const getWorkspace = async (workspace_id: string, userId: string) => {
   const res = await fetch(
